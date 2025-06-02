1adacb15c0bcf3669880437e4ed2906bdb37884f
nithesh1997 <nitheshroshan007@gmail.com>
2025-06-02 20:39:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+document.getElementById('btn').addEventListener('click',()=>{
+    let sumValue = sum(5,3)
+
+    document.getElementById('text-result').innerHTML = `Sum Values 5 + 3 = ${sumValue}`
+})
